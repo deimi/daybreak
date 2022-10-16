d26d00ff99c6b626d6c1a4e614ec818f04646581
BluemediaGER <oliver@traber-info.de>
2022-10-16 22:19:53 +0200

    Update translation

--- resources/views/time_trackings/time-tracking-manager.blade.php
+++ resources/views/time_trackings/time-tracking-manager.blade.php
@@ -3,7 +3,7 @@
     <div class="flex justify-between">
         <div class="bg-gray-50 lg:w-1/2">
             <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-                <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                <h2 class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
                 <span class="block">
                     {{ __(ucfirst($workingSession->status)) }}
                 </span>
